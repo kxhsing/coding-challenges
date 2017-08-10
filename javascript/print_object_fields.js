abaf650105722396a9525340d8203859c9df19f1
vagrant <kxhsing@gmail.com>
2017-08-10 19:15:43 +0000

    Created separate folder for challenges solved in js

---
+++ javascript/print_object_fields.js
@@ -0,0 +1,38 @@
+
+var people = {
+  "jesse": {"name": "jesse", "email": "jesse@domain.com"},
+  "karen": {"name": "karen", "email": "karen@domain.com"}
+  
+}
+
+function print_field_values(people) {
+
+var people_ids = Object.keys(people);
+var fields = Object.keys(people[people_ids[0]])
+
+
+output = {}
+
+for (i=0; i < fields.length; i++) {
+  results = [];
+  var field = fields[i];
+  
+  for (j=0; j < people_ids.length; j++){
+    var person_id = people_ids[j];
+    var person = people[person_id]; 
+    
+    results.push(person[field]);
+  }
+  output[fields[i]] = results;
+}
+
+
+
+console.log(output);
+
+}
+
+
+
+
+
